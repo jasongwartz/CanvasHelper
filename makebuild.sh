58c34c39dc4d7f6dc7dcc61ecc8f0615363163d9
Jason Gwartz <jason@jasongwartz.com>
2015-08-07 09:50:43 +0100

    Update makebuild.sh

--- makebuild.sh
+++ makebuild.sh
@@ -2,8 +2,4 @@
 
 # I use this to copy new builds into a seperate directory for testing or deployment
 
-<<<<<<< HEAD
 rsync -av . ../chnewbuild --exclude=makebuild.sh --exclude=*.pyc --exclude=logs/*.txt --exclude=*.csv --exclude=*/sensitive.json
-=======
-rsync -av . ../chnewbuild --exclude=*.pyc --exclude=logs/*.txt --exclude=*.csv --exclude=*/sensitive.json
->>>>>>> refs/remotes/origin/master
